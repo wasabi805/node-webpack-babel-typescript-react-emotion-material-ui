30cfa564fa6c120d058573c42817471dbd6df5b0
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-10-10 21:05:58 -0700

    started wirg up themeProvider.

--- src/providers/themeProvider.jsx
+++ src/providers/themeProvider.jsx
@@ -1,8 +1,18 @@
 
 
 import { createTheme } from '@mui/material'
+import red from '@material-ui/core/colors/red';
 // THIS IS BASICALLY WHERE YOU'RE GONNA CREATE THE DEFAULTS YOUR THINGS LIKE TYPOGRAPHY. 
 const theme = createTheme({
+    overrides: {
+        MuiButton: { // Name of the component ⚛️ / style sheet
+          text: { // Name of the rule
+            color: red, // Some CSS
+          },
+        },
+      },
+
+
     typography:{
         fontFamily: [].join(''),
         h1:{ color: 'magenta' ,
